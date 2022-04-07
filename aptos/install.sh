a3025fd24d8bea685396023b3cad644df47be2fc
Aleksandr Volkov <alexander.volkov@latoken.com>
2022-04-08 00:07:09 +0700

    fix aptos

--- aptos/install.sh
+++ aptos/install.sh
@@ -42,4 +42,4 @@ echo -e "\e[1m\e[32mTo check the Aptos Node Logs: \e[0m"
 echo -e "\e[1m\e[39m    docker logs -f aptos-fullnode-1 --tail 5000 \n \e[0m"
 
 echo -e "\e[1m\e[32mTo check the node status: \e[0m"
-echo -e "\e[1
+echo -e "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
--- utils/docker.sh
+++ utils/docker.sh
@@ -3,6 +3,6 @@ sudo apt update
 sudo apt install wget -y
 wget -O get-docker.sh https://get.docker.com
 sudo sh get-docker.sh
-sudo apt install -y docker-compose
+#sudo apt install -y docker-compose
 rm -f get-docker.sh
 sudo usermod -aG docker $USER
